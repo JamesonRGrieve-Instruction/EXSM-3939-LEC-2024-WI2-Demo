6b454d4554e93e3fce24178c09f4b49cf6cfa2dd
James Grieve <jgrieve@ualberta.ca>
2024-02-26 19:54:27 -0700

    Home page with list element (empty tag);

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,25 @@
 export default function HomePage() {
   return (
-   
+    <>
+      <main>
+        <h1>Hello, world!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          convallis ante ut magna condimentum, eget ornare ligula mattis.
+          Maecenas congue, sapien porta fermentum venenatis, lectus justo cursus
+          nisl, et accumsan dolor lacus ut dolor. Vivamus vulputate arcu a nulla
+          finibus rhoncus. Curabitur viverra est viverra risus interdum, ut
+          pellentesque lectus molestie. Ut consequat mauris a risus consequat,
+          vel semper turpis faucibus. Duis venenatis dolor in metus aliquam,
+          quis volutpat tortor aliquet. Cras volutpat velit nisl, vitae
+          sollicitudin nibh aliquam id. Sed blandit convallis massa, a blandit
+          quam fringilla eget. Mauris pretium enim quis pulvinar volutpat. Cras
+          arcu nisi, feugiat at elementum vitae, mattis sit amet quam. Nulla
+          interdum in lacus vitae consectetur. In lacinia ex quis mauris
+          vehicula lobortis.
+        </p>
+      </main>
+      <footer></footer>
+    </>
   );
 }
