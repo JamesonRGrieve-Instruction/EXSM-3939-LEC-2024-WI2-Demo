b564da05114e12a74d84ee97e5a9184748235314
James Grieve <jgrieve@ualberta.ca>
2024-03-08 19:49:57 -0700

    Improve CSS;

--- src/app/TextMechanic.jsx
+++ src/app/TextMechanic.jsx
@@ -44,18 +44,27 @@ export default function TextMechanic() {
   // I kept it in with an empty array as a demo of syntax.
   const objectMap = useCallback((object, fn) => Object.keys(object).map(key => fn(key, object[key])), [])
 
-  return <div className={styles.field}>
+  return <div className={styles.text_mechanic}>
     {showHeading ? <h1>Text Mechanic</h1> : null}
     <textarea onChange={(e) => {
       console.log("New Value: ", e.target.value);
       setInputValue(e.target.value);
     }} value={inputValue}></textarea>
-    <input id="field-a" value={fieldAValue} onChange={(event) => setFieldAValue(event.target.value)}></input>
-    <input id="field-b" value={fieldBValue} onChange={(event) => setFieldBValue(event.target.value)}></input>
+    <div>
+      <label htmlFor="field-a">Input Field A</label>
+      <label htmlFor="field-b">Input Field B</label>
+      <input id="field-a" value={fieldAValue} onChange={(event) => setFieldAValue(event.target.value)}></input>
+
+      <input id="field-b" value={fieldBValue} onChange={(event) => setFieldBValue(event.target.value)}></input>
+    </div>
+
     <div className={styles.control}>
       {objectMap(controls, (label, controlFunction) => <button key={label} onClick={(e) => { e.preventDefault(); controlFunction(); setCharInputValue(old => old + e.target.innerText[0]) }}>{label}</button>)}
     </div>
 
-    <input type="text" value={charInputValue} disabled />
+    <div>
+      <input type="text" value={charInputValue} disabled />
+    </div>
+
   </div>;
 }
