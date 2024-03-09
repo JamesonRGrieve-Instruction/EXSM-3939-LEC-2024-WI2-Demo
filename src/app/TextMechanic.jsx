f70a0dbf37de1675334d94d8292fc31fe61f2ecb
James Grieve <jgrieve@ualberta.ca>
2024-03-08 20:05:27 -0700

    Splitting memoized functions to be more efficient with dependencies;

--- src/app/TextMechanic.jsx
+++ src/app/TextMechanic.jsx
@@ -1,43 +1,58 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useEffect } from 'react';
 import styles from "./page.module.css";
+
 export default function TextMechanic() {
   const [inputValue, setInputValue] = useState("");
   const [showHeading, setShowHeading] = useState(true);
   const [charInputValue, setCharInputValue] = useState("");
   const [fieldAValue, setFieldAValue] = useState("");
   const [fieldBValue, setFieldBValue] = useState("");
-
+  useEffect(() => {
+    console.log("New Field A", fieldAValue)
+  }, [fieldAValue])
+  useEffect(() => {
+    console.log("New Field B", fieldBValue)
+  }, [fieldBValue])
   // useMemo stores a value (typically a constant) in memory. Similar to useEffect, useMemo has a dependency array - when any dependency changes, 
   // the value will be recalculated. If the array is empty it will be memoized for the lifespan of the component.
-  const controls = useMemo(() => ({
-    "Toggle Heading": () => {
-      setShowHeading(old => !old);
-    },
-    "Sort Lines": () => {
-      setInputValue(old => old.split("\n").sort().join("\n"));
-    },
-    "Number Lines": () => {
-      setInputValue(old => old.split("\n").map((line, index) => `${index + 1}. ${line}`).join("\n"));
-    },
-    "Un-Number Lines": () => {
-      setInputValue(old => old.split("\n").map((line) => (!isNaN(line[0]) && line[1] === "." && line[2] === " ") ? line.substring(3) : line).join("\n"));
-    },
-    "Remove Empty Lines": () => {
-      setInputValue(old => old.split("\n").filter(line => line.trim() !== "").join("\n"));
-    },
-    "To Uppercase": () => {
-      setInputValue(old => old.toUpperCase());
-    },
-    "To Lowercase": () => {
-      setInputValue(old => old.toLowerCase());
-    },
-    "Cipher Up": () => {
-      setInputValue(old => old.split('').map(char => char.match(/^[a-zA-Z]+$/i) ? (String.fromCharCode([90, 122].includes(char.charCodeAt(0)) ? char.charCodeAt(0) - 25 : char.charCodeAt(0) + 1)) : char).join(""));
-    },
-    "Cipher Down": () => {
-      setInputValue(old => old.split('').map(char => char.match(/^[a-zA-Z]+$/i) ? (String.fromCharCode([65, 97].includes(char.charCodeAt(0)) ? char.charCodeAt(0) + 25 : char.charCodeAt(0) - 1)) : char).join(""));
+  const staticControls = useMemo(() => {
+    console.log("Memoized controls changed.");
+    return {
+      "Toggle Heading": () => {
+        setShowHeading(old => !old);
+      },
+      "Sort Lines": () => {
+        setInputValue(old => old.split("\n").sort().join("\n"));
+      },
+      "Number Lines": () => {
+        setInputValue(old => old.split("\n").map((line, index) => `${index + 1}. ${line}`).join("\n"));
+      },
+      "Un-Number Lines": () => {
+        setInputValue(old => old.split("\n").map((line) => (!isNaN(line[0]) && line[1] === "." && line[2] === " ") ? line.substring(3) : line).join("\n"));
+      },
+      "Remove Empty Lines": () => {
+        setInputValue(old => old.split("\n").filter(line => line.trim() !== "").join("\n"));
+      },
+      "To Uppercase": () => {
+        setInputValue(old => old.toUpperCase());
+      },
+      "To Lowercase": () => {
+        setInputValue(old => old.toLowerCase());
+      },
+      "Cipher Up": () => {
+        setInputValue(old => old.split('').map(char => char.match(/^[a-zA-Z]+$/i) ? (String.fromCharCode([90, 122].includes(char.charCodeAt(0)) ? char.charCodeAt(0) - 25 : char.charCodeAt(0) + 1)) : char).join(""));
+      },
+      "Cipher Down": () => {
+        setInputValue(old => old.split('').map(char => char.match(/^[a-zA-Z]+$/i) ? (String.fromCharCode([65, 97].includes(char.charCodeAt(0)) ? char.charCodeAt(0) + 25 : char.charCodeAt(0) - 1)) : char).join(""));
+      },
     }
-  }), []);
+  }, []);
+  const dynamicControls = useMemo(() => ({
+    "Replace Field A with Field B": () => {
+      console.log("Field A", fieldAValue, "Field B", fieldBValue);
+      setInputValue(old => old.replaceAll(fieldAValue.trim(), fieldBValue.trim()));
+    }
+  }), [fieldAValue, fieldBValue])
 
   // useCallback works identically to useMemo, just with functions instead of values.
   // In this case, the function could've just been ejected from the component as it does not reference state.
@@ -59,7 +74,7 @@ export default function TextMechanic() {
     </div>
 
     <div className={styles.control}>
-      {objectMap(controls, (label, controlFunction) => <button key={label} onClick={(e) => { e.preventDefault(); controlFunction(); setCharInputValue(old => old + e.target.innerText[0]) }}>{label}</button>)}
+      {objectMap({ ...staticControls, ...dynamicControls }, (label, controlFunction) => <button key={label} onClick={(e) => { e.preventDefault(); controlFunction(); setCharInputValue(old => old + e.target.innerText[0]) }}>{label}</button>)}
     </div>
 
     <div>
