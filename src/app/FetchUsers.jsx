9b1c37575726e6c8298cfb58bd2d076a0c7acf58
James Grieve <jgrieve@ualberta.ca>
2024-03-11 20:27:26 -0600

    Basic SWR implementation;

--- src/app/FetchUsers.jsx
+++ src/app/FetchUsers.jsx
@@ -1,25 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
+import useSWR, { mutate } from "swr";
 export default function FetchUsers() {
-  const [userData, setUserData] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const response = await axios.get(
-        "https://randomuser.me/api/?results=5&inc=gender,name,location,email",
-      );
-      setUserData(response.data.results);
-    })();
-  }, []);
-  useEffect(() => {
-    console.log("User Data Was Updated: ", userData);
-  }, [userData]);
-  return (
-    <ul>
-      {userData.map((datum) => (
-        <li key={datum.email}>
-          {datum.name.first} {datum.name.last}
-        </li>
-      ))}
-    </ul>
+  const { data, isLoading, error } = useSWR(
+    "/users",
+    async () => {
+      return (
+        await axios.get(
+          "https://randomuser.me/api/?results=5&inc=gender,name,location,email",
+        )
+      ).data.results;
+    },
+    {
+      revalidateOnFocus: false,
+      onSuccess: (data) => {
+        console.log(data);
+      },
+      onError: () => {},
+    },
+  );
+  return isLoading ? (
+    <p>Loading users...</p>
+  ) : error ? (
+    <p>ERROR: {error.message}</p>
+  ) : (
+    <>
+      <ul>
+        {data.map((datum) => (
+          <li key={datum.email}>
+            {datum.name.first} {datum.name.last}
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => {
+          mutate("/users");
+        }}
+      >
+        Load More
+      </button>
+    </>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 import styles from "./page.module.css";
 import React from "react";
-import StatefulInput from "./StatefulInput";
 import FetchUsers from "./FetchUsers";
+import StatefulInput from "./StatefulInput";
 export default function Home() {
   return (
     <main className={styles.main}>
