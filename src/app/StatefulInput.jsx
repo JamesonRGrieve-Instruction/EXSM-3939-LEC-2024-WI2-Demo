60cf4e1db2b87e8b5578176fb368a6bf60e36dab
James Grieve <jgrieve@ualberta.ca>
2024-03-11 19:23:03 -0600

    useRef;

--- src/app/StatefulInput.jsx
+++ src/app/StatefulInput.jsx
@@ -1,12 +1,23 @@
 import React, { useState, useRef } from "react";
 export default function StatefulInput() {
   const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
   return (
-    <input
-      value={inputValue}
-      onChange={(event) => {
-        setInputValue(event.target.value);
-      }}
-    ></input>
+    <>
+      <input
+        value={inputValue}
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+        ref={inputRef}
+      ></input>
+      <button
+        onClick={() => {
+          inputRef.current.focus();
+        }}
+      >
+        Set Focus
+      </button>
+    </>
   );
 }
