3f10ce47b9feec876de262235b10ff7e671f882a
James Grieve <jgrieve@ualberta.ca>
2024-03-15 20:36:08 -0600

    Add comments, add .prettierrc;

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,19 @@ export default function Home() {
   return (
     <main>
       <Paragraph text="Hello, World!" />
+
+      {/* 
+      This form has its fields provided by the displayFields state of the page component.
+      As the value of the prop is state, a change to that state will force a component rerender, just like its own state would.
+      */}
       <Form formFields={displayFields} />
+
+      {/*
+      This form has its fields provided by the additionFields external constant (which should never mutate by the rules of react).
+      Its other props are also constants (despite the function referencing state, the function itself is contantly defined).
+      This means that the only thing that will cause a rerender of this component is a change to its own state.
+      Its constant props will not change and thus not drive rerenders.
+      */}
       <Form
         formFields={additionFields}
         submitLabel="Add Field"
