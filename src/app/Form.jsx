9271be5f06cedbc43cf8620c2f44d58f2e769ee0
James Grieve <jgrieve@ualberta.ca>
2024-03-15 20:03:41 -0600

    Render stateful inputs for each item in a prop array;

--- src/app/Form.jsx
+++ src/app/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 export default function Form({ formFields, submitLabel, submitFn }) {
   /*
     formFields = [
@@ -10,19 +10,42 @@ export default function Form({ formFields, submitLabel, submitFn }) {
     ]
 
     */
+
+  const [fieldValues, setFieldValues] = useState(
+    formFields.reduce((obj, field) => {
+      obj[field.name] = "";
+      return obj;
+    }, {})
+  );
+  // First Iteration: {} => {name: ""}
+  // Second Iteration: {name: ""} => {name: "", label: ""}
+  // Third Iteration: {name: "", label: ""} => {name: "", label: "", type: ""}
+  useEffect(() => {
+    console.log(fieldValues);
+  }, [fieldValues]);
   return (
     <form>
       {formFields.map((field) => (
         <div key={`${field.name}`}>
           <label htmlFor={field.name}>{field.label}</label>
-          <input id={field.name} type={field.type} />
+          <input
+            id={field.name}
+            value={fieldValues[field.name]}
+            onChange={(event) => {
+              setFieldValues((previous) => ({
+                ...previous,
+                [field.name]: event.target.value,
+              }));
+            }}
+            type={field.type}
+          />
         </div>
       ))}
       <button
         type="submit"
         onClick={(event) => {
           event.preventDefault();
-          submitFn();
+          submitFn(fieldValues);
         }}
       >
         {submitLabel}
