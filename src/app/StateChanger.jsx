3bc9bbf05771f2b1a35bb82acc09c545a38ea455
James Grieve <jgrieve@ualberta.ca>
2024-03-15 19:14:42 -0600

    Bonus points;

--- src/app/StateChanger.jsx
+++ src/app/StateChanger.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 export default function StateChanger() {
   const [state, setState] = useState(false);
+  const [shown, setShown] = useState(true);
+
   return (
     <>
-      <h1>State: {state ? "Changed" : "Original"}</h1>
+      {shown && <h1>State: {state ? "Changed" : "Original"}</h1>}
+      {/*shown ? <h1>State: {state ? "Changed" : "Original"}</h1> : null*/}
       <button
         onClick={(event) => {
           event.preventDefault();
@@ -11,6 +14,14 @@ export default function StateChanger() {
         }}
       >
         Change State
+      </button>
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          setShown((previous) => !previous);
+        }}
+      >
+        Show/Hide Text
       </button>
     </>
   );
