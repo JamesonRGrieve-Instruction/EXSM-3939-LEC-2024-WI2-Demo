808a94ce2bac3aedbec67522eba02a6cb960744f
James Grieve <jgrieve@ualberta.ca>
2024-03-18 19:53:09 -0600

    State-driven context with mutation;

--- src/app/Paragraph.jsx
+++ src/app/Paragraph.jsx
@@ -1,4 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+// In order to useContext, we must import the context we created higher in the hierarchy.
+import { ExampleContext } from "./page";
 export default function Paragraph({ text }) {
-  return <p>{text}</p>;
+  // You can use useContext() directly in the JSX, but it will not like it if it is conditionally rendered, or if it is used multiple times. Therefore, typically best to assign it to a constant.
+  const contextValue = useContext(ExampleContext);
+  return (
+    <div>
+      <p>
+        {/* To get the value of a state-driven context, reference whatever key of the context object to which you assigned the state value reference. */}
+        {text}Context: {contextValue.data}
+      </p>
+      <button
+        onClick={() => {
+          // To change the value of a state-driven context, call the key of the context object to which you assigned the setState function (in the exact same way you would call said setState function).
+          contextValue.mutate((previous) => previous + "!");
+        }}
+      >
+        Add Emphasis
+      </button>
+    </div>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import Paragraph from "./Paragraph";
 import Form from "./Form";
 const additionFields = [
@@ -7,34 +7,45 @@ const additionFields = [
   { name: "label", label: "Label", type: "text" },
   { name: "type", label: "Type", type: "text" },
 ];
+// When creating context, create it outside the component so that it isn't reinitalized every render and ensure to export it. In JavaScript, it can be an empty argument list (only TypeScript gets upset).
+export const ExampleContext = createContext();
 export default function Home() {
   const [displayFields, setDisplayFields] = useState([
     { name: "email", label: "E-Mail", type: "text" },
     { name: "password", label: "Password", type: "password" },
   ]);
+  // With this context we are providing a way to change the value as well as read the value, so we are binding it to a state.
+  const [exampleText, setExampleText] = useState("Hello, World!");
   return (
-    <main>
-      <Paragraph text="Hello, World!" />
+    // Everything within the provider has access to the provider's value through a useContext() hook.
+    // The value prop of the provider sets the initial value of the context, in this case bound to the state via reference.
+    <ExampleContext.Provider
+      value={{ data: exampleText, mutate: setExampleText }}
+      /* value="Hello, World!" */
+    >
+      <main>
+        <Paragraph text="Hello, World!" />
 
-      {/* 
+        {/* 
       This form has its fields provided by the displayFields state of the page component.
       As the value of the prop is state, a change to that state will force a component rerender, just like its own state would.
       */}
-      <Form formFields={displayFields} />
+        <Form formFields={displayFields} />
 
-      {/*
+        {/*
       This form has its fields provided by the additionFields external constant (which should never mutate by the rules of react).
       Its other props are also constants (despite the function referencing state, the function itself is contantly defined).
       This means that the only thing that will cause a rerender of this component is a change to its own state.
       Its constant props will not change and thus not drive rerenders.
       */}
-      <Form
-        formFields={additionFields}
-        submitLabel="Add Field"
-        submitFn={(formData) => {
-          setDisplayFields((previous) => [...previous, formData]);
-        }}
-      />
-    </main>
+        <Form
+          formFields={additionFields}
+          submitLabel="Add Field"
+          submitFn={(formData) => {
+            setDisplayFields((previous) => [...previous, formData]);
+          }}
+        />
+      </main>
+    </ExampleContext.Provider>
   );
 }
