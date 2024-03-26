9eb687f989650904257a31228ca8979ec0f2fe9c
James Grieve <jgrieve@ualberta.ca>
2024-03-25 20:16:58 -0600

    Additional main.js config;

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,24 @@
+import { features } from 'process';
+
 const path = require('path');
 
 const config = {
   stories: ['../src/Application.mdx', '../src/app/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)', '../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-styling', '@storybook/addon-docs'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-docs'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+  core: { disableTelemetry: true },
   docs: {
     autodocs: 'tag',
     defaultName: 'Documentation',
   },
+  refs: {
+    example: { title: 'JRGComponents', url: 'https://jamesonrgrieve.github.io/jrgcomponents', expanded: false },
+  },
+  logLevel: 'debug',
+  env: (config) => ({ ...config, APP_NAME: 'Example' }),
   staticDirs: [],
   webpackFinal: async (config, { configType }) => {
     if (config.resolve)
