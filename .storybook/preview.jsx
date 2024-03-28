0be079e088680c6a14731b9c608d7a2cef295221
James Grieve <jgrieve@ualberta.ca>
2024-03-28 17:15:23 -0600

    Center story wrapper;

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -26,3 +26,23 @@ const preview = {
 };
 
 export default preview;
+export const decorators = [
+  (Story, context) => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          position: 'absolute',
+          top: '0',
+          right: '0',
+          bottom: '0',
+          left: '0',
+        }}
+      >
+        <Story />
+      </div>
+    );
+  },
+];
