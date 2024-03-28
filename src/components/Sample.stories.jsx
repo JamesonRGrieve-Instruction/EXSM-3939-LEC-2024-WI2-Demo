2ad914a659e394ad3f43262c64d1b37861ca9325
James Grieve <jgrieve@ualberta.ca>
2024-03-28 17:37:55 -0600

    Radio button choices for functions;

--- src/components/Sample.stories.jsx
+++ src/components/Sample.stories.jsx
@@ -11,6 +11,7 @@ export default {
     heading: { control: 'text' },
     bodyText: { control: 'text' },
     buttonText: { control: 'text' },
+    functionKey: { control: 'radio', options: ['logClicked', 'logEvent'] },
   },
   parameters: {
     componentSubtitle: 'A Sample Component For Learning Storybook',
@@ -35,11 +36,17 @@ export default {
   },
 };
 
+const onClickFunctions = {
+  logEvent: (event) => console.log(event),
+  logClicked: () => console.log('Clicked!'),
+};
+
 // Named export for each story
-export const Sample = (args) => <SampleComponent {...args} />;
+export const Sample = (args) => <SampleComponent heading={args.heading} bodyText={args.bodyText} buttonText={args.buttonText} onClick={onClickFunctions[args.functionKey]} />;
 
 // Default values for props
 Sample.args = {
+  functionKey: 'log',
   heading: 'Sample Component',
   bodyText:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
