c2857d5ce5b682900ef39e162dfbcca4ba4355d9
James Grieve <jgrieve@ualberta.ca>
2024-04-02 17:14:44 -0600

    Comparison of MUI with vanilla HTML;

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,22 @@
 import React from 'react';
+import { Box, TextField, Button } from '@mui/material';
 export default function Home() {
-  return <main></main>;
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        width: '100%',
+      }}
+    >
+      <Box component='main'>
+        <TextField variant='outlined' />
+        <Button variant='contained'>Submit</Button>
+      </Box>
+      <main>
+        <input type='text' />
+        <button>Submit</button>
+      </main>
+    </div>
+  );
 }
