45354ca06486e9203014a07739f198ea18906a87
James Grieve <jgrieve@ualberta.ca>
2024-04-05 19:25:03 -0600

    AppBar with nav links;

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import React from 'react';
 import ThemeWrapper from './ThemeWrapper';
+import { AppBar, Typography, Box, List, ListItem } from '@mui/material';
+import Link from 'next/link';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -13,7 +15,28 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <ThemeWrapper>{children}</ThemeWrapper>
+        <ThemeWrapper>
+          <AppBar sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'row' }}>
+            <Box component='nav' flex='1 1 25%'>
+              <List sx={{ display: 'flex', justifyContent: 'center' }}>
+                <ListItem sx={{ width: 'unset' }}>
+                  <Link href='/'>Home</Link>
+                </ListItem>
+                <ListItem sx={{ width: 'unset' }}>
+                  <Link href='/about'>About</Link>
+                </ListItem>
+                <ListItem sx={{ width: 'unset' }}>
+                  <Link href='/contact'>Contact</Link>
+                </ListItem>
+              </List>
+            </Box>
+            <Typography variant='h3' component='h1' textAlign='center'>
+              UAlberta AppDev
+            </Typography>
+            <Box flex='1 1 25%'></Box>
+          </AppBar>
+          {children}
+        </ThemeWrapper>
       </body>
     </html>
   );
